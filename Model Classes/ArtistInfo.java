5da962891223b84c3f645afc27e7d08a9ae3144e
Sai Samarth <sai003sam@gmail.com>
2021-07-29 20:08:09 -0500

    Update ArtistInfo.java

--- Model Classes/ArtistInfo.java
+++ Model Classes/ArtistInfo.java
@@ -14,7 +14,10 @@ public class ArtistInfo {
 	private String name;
 	private String email;
 	
-
+	 public ArtistInfo(String name, String email) {
+	        this.name = name;
+	        this.email = email;
+	    }
 	
 	public String getName() {
 		return name;
