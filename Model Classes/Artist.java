e8e19b929a7a72a3445b69e5576be81fa9c33175
Sai Samarth <sai003sam@gmail.com>
2021-07-29 19:16:33 -0500

    Add files via upload

---
+++ Model Classes/Artist.java
@@ -0,0 +1,49 @@
+/**
+ * Artist object
+ * 
+ * @author Sai Samarth
+ * 
+ */
+public class Artist {
+	/*
+	 * Correspond to the artist table
+	 */
+	
+	private String id;
+	private String name; 
+	private String keyword;
+	
+
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "User [ id=" + id + ", name="
+				+ name + ", keyword=" + keyword +"]";
+	}
+}
+
---
+++ Model Classes/Playlist.java
@@ -0,0 +1,33 @@
+
+public class Playlist {
+	/*
+	 * Correspond to the playlist table
+	 */
+	
+	private String id;
+	private String song_id;
+	
+
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSong_Id() {
+		return song_id;
+	}
+
+	public void setSong_Id(String song_id) {
+		this.song_id = song_id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Playlist [ id=" + id + ", song id="
+				+ song_id + "]";
+	}
+}
