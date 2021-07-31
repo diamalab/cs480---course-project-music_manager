47010eb47760650f72bbc8508fe224a744d348c1
Sai Samarth <sai003sam@gmail.com>
2021-07-30 19:04:21 -0500

    Update PlaylistSongs.java

--- Model Classes/PlaylistSongs.java
+++ Model Classes/PlaylistSongs.java
@@ -4,38 +4,38 @@ public class PlaylistSongs {
 	 * Correspond to the playlist_songs table
 	 */
 	
-    private int playlist_songs_id;
-	private int playlist_id;
-	private int song_id;
+    private String playlist_songs_id;
+	private String playlist_id;
+	private String song_id;
 	
-	 public Playlist(int playlist_songs_id, int playlist_id, int song_id) {
+	 public Playlist(String playlist_songs_id, String playlist_id, String song_id) {
 	       this.playlist_songs_id = playlist_songs_id;
            this.playlist_id = playlist_id;
            this.song_id = song_id;
 	    }
 
 	
-	public int getPlaylistSongsId() {
+	public String getPlaylistSongsId() {
 		return playlist_songs_id;
 	}
 
-	public void setPlaylistSongsId(int id) {
+	public void setPlaylistSongsId(String id) {
 		this.playlist_songs_id = id;
 	}
 
-	public int getPlaylistId() {
+	public String getPlaylistId() {
 		return playlist_id;
 	}
 
-	public void setPlaylist_Id(int id) {
+	public void setPlaylist_Id(String id) {
 		this.playlist_id = id;
 	}
 
-    public int getSongID() {
+    public String getSongID() {
         return song_id;
     }
 
-    public void setSongId(int id) {
+    public void setSongId(String id) {
         this.song_id = id;
     }
 
